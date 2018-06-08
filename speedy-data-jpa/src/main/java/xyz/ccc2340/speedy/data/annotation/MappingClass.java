dea6bccc0e8a37b883b02595172510c60491da7a
cgx <1045550775@qq.com>
2018-06-08 21:46:03 +0800

    修改说明

--- speedy-data-jpa/src/main/java/xyz/ccc2340/speedy/data/annotation/MappingClass.java
+++ speedy-data-jpa/src/main/java/xyz/ccc2340/speedy/data/annotation/MappingClass.java
@@ -3,7 +3,7 @@ package xyz.ccc2340.speedy.data.annotation;
 import java.lang.annotation.*;
 
 /**
- * @Description 用于QueryCondition，标记对应的实体类型
+ * @Description 用于标记对应的实体类型
  * @Author chenguangxue
  * @CreateDate 2018/06/08 15:12
  */
